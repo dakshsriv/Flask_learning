34d7ed2ac0410ef3c01ec1735ee4653f4eadf668
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2021-03-22 19:55:06 -0400

    Detailed view bug fixed

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -54,10 +54,11 @@ def get_post(id, check_author=True):
 
     if post is None:
         abort(404, "Post id {0} doesn't exist.".format(id))
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
+    try:
+        if check_author and post['author_id'] != g.user['id']:
+            abort(403)
+    except:
+        return post
     return post
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
