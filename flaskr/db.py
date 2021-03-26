4f0a951a641a6a95e6f2c6803c9f60c28b7cffae
Daksh Srivastava <daksh.srivastava.10@gmail.com>
2021-03-26 19:21:55 -0400

    removed venv from repo

--- flaskr/db.py
+++ flaskr/db.py
@@ -23,14 +23,11 @@ def close_db(e=None):
         db.close()
 
 
-
-
 def init_db():
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
+    with current_app.open_resource('schema1.sql') as f:
         db.executescript(f.read().decode('utf8'))
-
 
 @click.command('init-db')
 @with_appcontext
